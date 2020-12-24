dd5282ab3ff2d22ae26e78115a0e1bae7a089de2
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2020-12-25 03:56:57 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,12 @@ function 棒グラフ (数値: number) {
         }
     }
 }
+input.onButtonPressed(Button.A, function () {
+    tl += -5
+    tl表示 = 1
+    basic.pause(500)
+    tl表示 = 0
+})
 input.onSound(DetectedSound.Loud, function () {
     input.setSoundThreshold(SoundThreshold.Loud, 255)
     if (input.soundLevel() > 128) {
@@ -26,19 +32,34 @@ input.onSound(DetectedSound.Loud, function () {
         pins.digitalWritePin(DigitalPin.P1, 0)
         basic.pause(150)
     }
-    input.setSoundThreshold(SoundThreshold.Loud, 72)
+    input.setSoundThreshold(SoundThreshold.Loud, tl)
+})
+input.onButtonPressed(Button.AB, function () {
+    tl = 50
+    tl表示 = 1
+    basic.pause(500)
+    tl表示 = 0
+})
+input.onButtonPressed(Button.B, function () {
+    tl += 5
+    tl表示 = 1
+    basic.pause(500)
+    tl表示 = 0
 })
 let b = 0
 let g = 0
 let r = 0
 let voice = 0
+let tl表示 = 0
+let tl = 0
 let strip = neopixel.create(DigitalPin.P0, 4)
 strip.clear()
-input.setSoundThreshold(SoundThreshold.Loud, 72)
+tl = 50
 pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P12, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+input.setSoundThreshold(SoundThreshold.Loud, tl)
 basic.forever(function () {
     voice = input.soundLevel()
     if (pins.digitalReadPin(DigitalPin.P8) == 0) {
@@ -55,6 +76,8 @@ basic.forever(function () {
         watchfont.showNumber2(13)
     } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
         watchfont.showNumber2(16)
+    } else if (tl表示 != 0) {
+        watchfont.showNumber2(tl)
     } else {
         棒グラフ(voice)
     }
